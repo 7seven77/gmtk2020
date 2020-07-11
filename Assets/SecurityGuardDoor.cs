98212ac0b9f7b9382fa04eb58c63e4b7c46ffa53
shoppiboi <ahasan.shoisob@gmail.com>
2020-07-11 14:18:27 +0100

    Create SecurityGuardDoor.cs

---
+++ Assets/SecurityGuardDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityGuardDoor : DoorBehave
+{
+
+    bool HasKey(TestPlayerMovement tpm) 
+    {
+        return tpm.hasKey;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Unit"))
+        {
+            if (HasKey(other.gameObject.GetComponent<TestPlayerMovement>())) 
+            {
+                PlayAnimation(GetRelativePosition(other.transform.position));
+            }
+        }
+    }
+
+}
