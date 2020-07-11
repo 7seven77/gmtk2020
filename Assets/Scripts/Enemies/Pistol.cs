00ec0adad4a93029a5976b18c737279c02ab4789
Zain Raja <zraja1105@gmail.com>
2020-07-11 20:19:30 +0100

    Add gun types

---
+++ Assets/Scripts/Enemies/Pistol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Fire()
+    {
+        if (weaponCooldown < 1)
+        {
+            CreateBullet();
+            weaponCooldown = weaponRate;
+        }
+    }
+}
--- Assets/Scripts/Enemies/Weapon.cs
+++ Assets/Scripts/Enemies/Weapon.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
     [SerializeField]
-    private GameObject bulletType = null;
+    protected GameObject bulletType = null;
 
-    private float weaponCooldown = 0;
+    protected float weaponCooldown = 0;
 
     [SerializeField]
-    private float weaponRate = 0;
+    protected float weaponRate = 0;
 
-    public void Fire()
-    {
-        if (weaponCooldown < 1)
-        {
-            GameObject bullet = Instantiate(bulletType, transform.position, transform.rotation);
-            bullet.GetComponent<Bullet>().Initialise(transform.eulerAngles.z);
-            weaponCooldown = weaponRate;
-        }
-    }
+    [SerializeField]
+    protected float spray = 0;
+
+    public abstract void Fire();
 
     private void Update()
     {
         weaponCooldown = Mathf.Max(weaponCooldown - 1, 0);
+    }
+
+    protected void CreateBullet()
+    {
+        GameObject bullet = Instantiate(bulletType, transform.position, transform.rotation);
+        bullet.GetComponent<Bullet>().Initialise(transform.eulerAngles.z + Random.Range(-spray, spray));
     }
 }
