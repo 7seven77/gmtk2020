9e33d091cd1503be42001d12126445c572cbb5af
Zain Raja <zraja1105@gmail.com>
2020-07-11 13:07:18 +0100

    Add basic enemy class

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected State state = State.Passive;
+
+    [SerializeField]
+    protected float movementSpeed = 1;
+
+    protected virtual void PassiveState()
+    {
+        // Do nothing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Passive)
+        {
+            PassiveState();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    Passive, // The object does not know the player exists and acts naturally
+    Chasing, // The object can see the player and is pursuing it
+    Searching, // The object no longer sees the player and is looking for them
+    Running, // The object is running from the player
+}
