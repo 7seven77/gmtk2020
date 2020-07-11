e20c91484a7b7ee4a72857b263c2125c8158504c
shoppiboi <ahasan.shoisob@gmail.com>
2020-07-11 22:40:16 +0100

    Create TakeOverTarget.cs
    handles the targetting of potential take over targets with the mouse

---
+++ Assets/TakeOverTarget.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeOverTarget : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+
+    public GameObject highlightSquare;
+
+    bool highlighted;
+
+    private void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        sr.color = Color.yellow;
+        highlightSquare.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        sr.color = Color.red;
+    }
+
+    public void TurnOffSquare() 
+    {
+        highlightSquare.SetActive(false);
+    }
+
+    public void TakeOver() 
+    {
+        this.gameObject.AddComponent<TestPlayerMovement>();
+        this.gameObject.GetComponent<TestPlayerMovement>().highlightSquare = highlightSquare;
+        Destroy(this);
+    }
+}
