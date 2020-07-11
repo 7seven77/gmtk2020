e116ace1f66192ae4446e42752fc61a582fae55d
shoppiboi <ahasan.shoisob@gmail.com>
2020-07-11 14:48:24 +0100

    Create BrownDoor.cs

---
+++ Assets/BrownDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownDoor : DoorBehave
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Unit"))
+        {
+            PlayAnimation(GetRelativePosition(other.transform.position));
+        }
+    }
+}
