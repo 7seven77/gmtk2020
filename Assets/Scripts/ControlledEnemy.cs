4c15efd9a8e7fe80a0866685b6a77a6bbfd6ba35
Zain Raja <zraja1105@gmail.com>
2020-07-12 15:37:10 +0100

    Add enemy detection

--- Assets/Scripts/ControlledEnemy.cs
+++ Assets/Scripts/ControlledEnemy.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class ControlledEnemy : Enemy
 {
-    float duration = 1000;
+    float duration = 100000;
 
     Vector2 targetPosition;
-
+    private bool moving;
     protected override void Start()
     {
         base.Start();
         targetPosition = transform.position;
+        moving = false;
     }
     // Update is called once per frame
     void Update()
@@ -24,8 +25,39 @@ public class ControlledEnemy : Enemy
         if (Input.GetMouseButtonDown(1))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
         }
-        MoveToPoint(targetPosition);
+        if ((Vector2) transform.position == targetPosition)
+        {
+            moving = false;
+        }
+        if (moving)
+        {
+            MoveToPoint(targetPosition);
+        }
+        else
+        {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
+            GameObject closest = null;
+            float distance = Mathf.Infinity;
+            Vector3 position = transform.position;
+            foreach (GameObject go in objects)
+            {
+                print(go);
+                Vector3 diff = go.transform.position - position;
+                float curDistance = diff.magnitude;
+                if (curDistance < distance && curDistance < viewRange)
+                {
+                    closest = go;
+                    distance = curDistance;
+                }
+            }
+            if (closest != null)
+            {
+                print(closest);
+                MoveToPoint(closest.transform.position);
+            }
+        }
     }
 
 }
